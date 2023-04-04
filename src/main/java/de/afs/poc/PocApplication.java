d88995e9ccfd9d48f43e40a6e8036a4c1a70a2b3
McAlm <stefan.wiese@camunda.com>
2023-04-04 17:01:08 +0200

    fips1-2-fips2

--- src/main/java/de/afs/poc/PocApplication.java
+++ src/main/java/de/afs/poc/PocApplication.java
@@ -8,7 +8,7 @@ import io.camunda.zeebe.spring.client.annotation.Deployment;
 
 @SpringBootApplication
 @EnableZeebeClient
-@Deployment(resources = {"classpath:foo.bpmn"})
+@Deployment(resources = {"classpath:*.bpmn"})
 public class PocApplication {
 
 	public static void main(String[] args) {
